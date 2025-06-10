d1fe314de5572990cf6769e7ba025a4a823dd982
AgarOther <AgarOther@hotmail.com>
2025-06-10 14:34:01 +0200

    IT FUCKING WORKS

--- Makefile
+++ Makefile
@@ -2,17 +2,25 @@ all:
 	@mkdir -p /home/scraeyme/data/mariadb
 	@mkdir -p /home/scraeyme/data/nginx
 	@mkdir -p /home/scraeyme/data/wordpress
+	@docker compose -f srcs/docker-compose.yml up -d --build
+
+up:
 	@docker compose -f srcs/docker-compose.yml up -d
 
-stop:
+down:
 	@docker compose -f srcs/docker-compose.yml down
 
-clean: stop
+restart: down up
+
+clean:
+	@docker compose -f srcs/docker-compose.yml down -v --remove-orphans
 	@rm -rf /home/scraeyme/data
 
 fclean: clean
-	@docker system prune -a -f
+	@docker system prune -af --volumes
 
 re: fclean all
+
+reclean: clean all
 
 .PHONY: all fclean clean re
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,15 +2,19 @@
 
 set -eu
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+if ! ls | grep wp-cli.phar; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+fi
 
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_ADMIN_NAME --dbpass=$DB_ADMIN_PASSWORD --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_NAME \
-	--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
-./wp-cli.phar user create $WP_USER_NAME $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-./wp-cli.phar user set-role 2 editor --allow-root
-./wp-cli.phar theme install blogair --activate --allow-root
+if ! ls | grep index.php; then
+	./wp-cli.phar core download --allow-root
+	./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_ADMIN_NAME --dbpass=$DB_ADMIN_PASSWORD --dbhost=mariadb --allow-root
+	./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_NAME \
+		--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
+	./wp-cli.phar user create $WP_USER_NAME $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+	./wp-cli.phar user set-role 2 editor --allow-root
+	./wp-cli.phar theme install blogair --activate --allow-root
+fi
 
-exec "$@"
+php-fpm7.4 -F
